14ed6f7991e77f2b9cc21ca49c1247b8496ab9de
Marcio Jose Martins Camara <118389170+marciomartins12@users.noreply.github.com>
2023-05-19 02:07:49 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const trocando= document.querySelector(".lua_e_sol");
+const body= document.querySelector("body");
+trocando.addEventListener("click", ()=>{
+    body.classList.toggle("dark_mode")
+})
